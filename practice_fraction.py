bb5953c7341638a2a306b654f85b5d807673090c
Margaret <margaritasobcenko@gmail.com>
2024-10-10 21:23:30 +0300

    HOW GET PRINT

--- practice_fraction.py
+++ practice_fraction.py
@@ -12,30 +12,40 @@ class Fraction:
         self.denumirator = denominator
         self.simplify()
 
+    def simplify(self):
+    #скорочення
+        gcd = math.gcd(self.numerator, self.denominator)
+        self.numerator //= gcd
+        self.denominator //= gcd
+
     def display(selfs):
-        print(f"{selfs.numerator}/{selfs.denominator}")
+        print(f"{selfs.numerator}/{selfs.denominator}")#показати дріб
 
     def to_decimal(self):
         return self.numerator / self.denominator# десяткове значення
 
     def divide(self, other):
-        if other.numerator == 0:
+        if other.numerator == 0:#ділення
             raise ValueError("Ділення на нуль неможливе.")
         new_numerator = self.numerator * other.denominator
         new_denominator = self.denominator * other.numerator
         return Fraction(new_numerator, new_denominator)
 
-    def multiply(self, other):
+    def multiply(self, other):#множення
         new_numerator = self.numerator * other.numerator
         new_denominator = self.denominator * other.denominator
         return Fraction(new_numerator, new_denominator)
 
-    def add(self, other):
+    def add(self, other):#додавання
         new_numerator = self.numerator * other.denominator + other.numerator * self.denominator
         new_denominator = self.denominator * other.denominator
         return Fraction(new_numerator, new_denominator)
 
-    def subtract(self, other):
+    def subtract(self, other):#віднімання
         new_numerator = self.numerator * other.denominator - other.numerator * self.denominator
         new_denominator = self.denominator * other.denominator
         return Fraction(new_numerator, new_denominator)
+
+fraction1 = Fraction(5,7)
+
+fraction2 = Fraction(3,8)
