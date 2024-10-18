00e46f1b3c225e1dd2ae2042ebe8e649d73b5a7e
Margaret <margaritasobcenko@gmail.com>
2024-10-18 15:39:13 +0300

    add classes Ship with inh., Frigate Destroyer Cruiser

--- inheritance_pr.py
+++ inheritance_pr.py
@@ -79,3 +79,65 @@ print(blender.blend())
 print(blender.turn_on())
 print(meat_grinder.grind_meat())
 print(meat_grinder.turn_on())
+
+
+#pr 2 Ship
+class Ship:
+    def __init__(self, length, width, color, engine):
+        self.length = length
+        self.width = width
+        self.color = color
+        self.engine = engine
+
+    def get_info(self):
+        return f"Length: {self.length}, Width: {self.width}, Color: {self.color}, Engine: {self.engine}"
+
+    def start_engine(self):
+        return f"{self.color} ship's engine is starting!"
+
+
+class Frigate(Ship):
+    def __init__(self, length, width, color, engine, ship_type):
+        super().__init__(length, width, color, engine)
+        self.ship_type = ship_type
+
+    def get_info(self):
+        return (super().get_info() +
+                f", Type: {self.ship_type}")
+
+
+class Destroyer(Ship):
+    def __init__(self, length, width, color, engine, num_missiles):
+        super().__init__(length, width, color, engine)
+        self.num_missiles = num_missiles
+
+    def get_info(self):
+        return (super().get_info() +
+                f", Number of missiles: {self.num_missiles}")
+
+
+class Cruiser(Ship):
+    def __init__(self, length, width, color, engine, num_crew):
+        super().__init__(length, width, color, engine)
+        self.num_crew = num_crew
+
+    def get_info(self):
+        return (super().get_info() +
+                f", Number of crew: {self.num_crew}")
+
+
+
+frigate = Frigate(100, 20, "Gray", "Turbojet", "Military")
+destroyer = Destroyer(120, 25, "Black", "Diesel", 30)
+cruiser = Cruiser(150, 30, "Blue", "Nuclear", 1500)
+
+
+print(frigate.get_info())
+print(frigate.start_engine())
+
+print(destroyer.get_info())
+print(destroyer.start_engine())
+
+print(cruiser.get_info())
+print(cruiser.start_engine())
+
