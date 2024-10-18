a6f226a80587138f56f089758f10478ad9b194c6
Margaret <margaritasobcenko@gmail.com>
2024-10-18 15:41:51 +0300

    add classes Ship with inh., Frigate Destroyer Cruiser  class Money  class Device

--- inheritance_pr.py
+++ inheritance_pr.py
@@ -1,31 +1,31 @@
-# class Money:
-#     def __init__(self,whole_money, coin_money,currency="USD"):
-#         self.whole_money = whole_money
-#         self.coin_money = coin_money
-#         if 0 <= coin_money < 100:
-#             self.coin_money = coin_money
-#             self.currency = currency
-#     def display(self):
-#         print(f"{self.whole_money}{self.currency} {self.coin_money} peny")
-#
-#     def add_money(self,money,coins):
-#         oll_coins = self.coin_money + coins
-#         oll_money = self.whole_money +money
-#         if  oll_coins >=100:
-#             oll_money += oll_coins //100
-#             oll_coins = oll_coins % 100
-#
-#         self.whole_money = oll_money
-#         self.coin_money = oll_coins
-#
-#     def get_money(self):
-#         return self.whole_money+self.coin_money/ 100
-#
-# my_money = Money(10,25)
-# my_money.display()
-# my_money.add_money(3,75)
-# my_money.display()
-# print("extended amount :",my_money.get_money())
+class Money:
+    def __init__(self,whole_money, coin_money,currency="USD"):
+        self.whole_money = whole_money
+        self.coin_money = coin_money
+        if 0 <= coin_money < 100:
+            self.coin_money = coin_money
+            self.currency = currency
+    def display(self):
+        print(f"{self.whole_money}{self.currency} {self.coin_money} peny")
+
+    def add_money(self,money,coins):
+        oll_coins = self.coin_money + coins
+        oll_money = self.whole_money +money
+        if  oll_coins >=100:
+            oll_money += oll_coins //100
+            oll_coins = oll_coins % 100
+
+        self.whole_money = oll_money
+        self.coin_money = oll_coins
+
+    def get_money(self):
+        return self.whole_money+self.coin_money/ 100
+
+my_money = Money(10,25)
+my_money.display()
+my_money.add_money(3,75)
+my_money.display()
+print("extended amount :",my_money.get_money())
 
 
 #PR1 Device
